915e84ea0ea189f4fc5830bbe124acc0fe34e631
csrhui <121076878+csrhui@users.noreply.github.com>
2025-08-19 23:28:56 +0800

    Add method listUserTasks, addUserTask
    Add the ability to store user-inputted text and display them back to user when requested.

--- src/main/java/Greeter.java
+++ src/main/java/Greeter.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 
 public class Greeter {
     static String botName = "Penny";
+    ArrayList<String> userTasks = new ArrayList<>();
 
     public void sayHi() {
         System.out.println("Hi! I'm " + Greeter.botName + ".\nWhat can I do for you?\n");
@@ -10,6 +12,24 @@ public class Greeter {
 
     public void sayBye() {
         System.out.println("Bye. Hope to see you again soon!\n");
+    }
+
+    private void listUserTasks() {
+        int taskCount = userTasks.size();
+        if (taskCount == 0) {
+            System.out.println("You have no tasks.");
+        } else {
+            StringBuilder taskList = new StringBuilder("Your tasks:\n");
+            for (int i = 0; i < taskCount; i++) {
+                taskList.append(i + 1).append(". ").append(userTasks.get(i)).append("\n");
+            }
+            System.out.println(taskList.toString());
+        }
+    }
+
+    private void addUserTask(String task) {
+        userTasks.add(task);
+        System.out.println("Added: " + task);
     }
 
     public void startConversation() {
@@ -26,7 +46,12 @@ public class Greeter {
                 break;
             }
 
-            System.out.println(userInput);
+            if (userInput.equalsIgnoreCase("list")) {
+                greeter.listUserTasks();
+                continue;
+            }
+
+            greeter.addUserTask(userInput);
         }
     }
 
