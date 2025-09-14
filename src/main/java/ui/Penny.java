ac1945367890356cafb0691da55da73ab519ce08
csrhui <121076878+csrhui@users.noreply.github.com>
2025-09-14 12:33:44 +0800

    Delete unused code

--- src/main/java/ui/Penny.java
+++ src/main/java/ui/Penny.java
@@ -1,7 +1,6 @@
 package ui;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 
 import command.Command;
@@ -10,34 +9,16 @@ import storage.Storage;
 
 
 public class Penny {
-//    Scanner scanner = new Scanner(System.in);
-//    InputParser inputParser = new InputParser();
-//    String logo =
-//            """
-//                     ____
-//                    |  _ \\ ___ _ __  _ __  _   _\s
-//                    | |_) / _ \\ '_ \\| '_ \\| | | |
-//                    |  __/  __/ | | | | | | |_| |
-//                    |_|   \\___|_| |_|_| |_|\\__, |
-//                                           |___/
-//                    """;
-
     public String getResponse(String userInput) {
         do {
             Command command = InputParser.parseCommand(userInput);
+            Storage storage = new Storage(Command.taskList);
+            try {
+                storage.saveTasks();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
             return command.respond();
         } while (!Command.shouldExit);
     }
-
-//    public static void main(String[] args) {
-//        Penny penny = new Penny();
-//        System.out.printf("Hello! I'm\n%s\nWhat can I do for you?\n%n", bot.logo);
-//        penny.getResponse();
-//        Storage storage = new Storage(Command.taskList);
-//        try {
-//            storage.saveTasks();
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
 }
